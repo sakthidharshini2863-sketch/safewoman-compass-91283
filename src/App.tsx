93c2536c4d206ee2000a5d338963c96a199e8bb7
sakthidharshini2863-sketch <sakthidharshini2863@gmail.com>
2025-10-08 01:07:42 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Articles from "./pages/Articles";
-import TransportFinder from "./pages/TransportFinder";
+import TransportFinder from "./pages/TransportFinder.tsx";
 import FriendlyStays from "./pages/FriendlyStays";
 import SafeRouteDemo from "./pages/SafeRouteDemo";
 import NotFound from "./pages/NotFound";
